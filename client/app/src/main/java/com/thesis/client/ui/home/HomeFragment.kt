eaae6567ede88d3f9831dcd7735bf4d572e7e697
André Schwarz <andre.schwarz@uxma.com>
2021-12-27 16:55:50 +0100

    first view implementation

--- client/app/src/main/java/com/thesis/client/ui/home/HomeFragment.kt
+++ client/app/src/main/java/com/thesis/client/ui/home/HomeFragment.kt
@@ -4,37 +4,36 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import com.thesis.client.R
 import com.thesis.client.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
 
+    // region Fields
+
     private lateinit var homeViewModel: HomeViewModel
     private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-    private val binding get() = _binding!!
+    // endregion
+
+    // region Lifecycle
 
     override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
-    ): View? {
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
         homeViewModel =
-                ViewModelProvider(this).get(HomeViewModel::class.java)
+            ViewModelProvider(this).get(HomeViewModel::class.java)
 
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
-        val root: View = binding.root
+        val root: View = binding!!.root
 
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
+        bindViewData()
+
         return root
     }
 
@@ -42,4 +41,62 @@ class HomeFragment : Fragment() {
         super.onDestroyView()
         _binding = null
     }
+
+    // endregion
+
+    // region Private Methods
+    private fun bindViewData() {
+        binding?.let { binding ->
+            homeViewModel.loadDataButtonText.observe(viewLifecycleOwner, {
+                binding.buttonLoadData.text = it
+            })
+            homeViewModel.establishConnectionButtonText.observe(viewLifecycleOwner, {
+                binding.buttonEstablishConnection.text = it
+            })
+            homeViewModel.startTrainingButtonText.observe(viewLifecycleOwner, {
+                binding.buttonStartTraining.text = it
+            })
+
+            context?.let { context ->
+                homeViewModel.loadDataImageDrawable.observe(viewLifecycleOwner, {
+                    binding.imageLoadData.setImageDrawable(
+                        ContextCompat.getDrawable(
+                            context,
+                            it
+                        )
+                    )
+                })
+
+                homeViewModel.establishConnectionImageDrawable.observe(viewLifecycleOwner, {
+                    binding.imageEstablishConnection.setImageDrawable(
+                        ContextCompat.getDrawable(
+                            context,
+                            it
+                        )
+                    )
+                })
+
+                homeViewModel.startTrainingImageDrawable.observe(viewLifecycleOwner, {
+                    binding.imageStartTraining.setImageDrawable(
+                        ContextCompat.getDrawable(
+                            context,
+                            it
+                        )
+                    )
+                })
+            }
+
+            binding.buttonLoadData.setOnClickListener {
+                homeViewModel.handleLoadDataButton()
+            }
+            binding.buttonEstablishConnection.setOnClickListener {
+                homeViewModel.handleEstablishConnectionButton()
+            }
+            binding.buttonStartTraining.setOnClickListener {
+                homeViewModel.handleStartTrainingButton()
+            }
+        }
+    }
+
+    // endegion
 }
--- client/app/src/main/java/com/thesis/client/ui/home/HomeViewModel.kt
+++ client/app/src/main/java/com/thesis/client/ui/home/HomeViewModel.kt
@@ -3,11 +3,66 @@ package com.thesis.client.ui.home
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.thesis.client.R
 
 class HomeViewModel : ViewModel() {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
+    companion object {
+        const val CROSS_DRAWABLE_ID = R.drawable.cross
+        const val CHECKMARK_DRAWABLE_ID = R.drawable.checkmark
     }
-    val text: LiveData<String> = _text
+
+    // region Button Texts
+
+    private val _loadDataButtonText = MutableLiveData<String>().apply {
+        value = "Datensatz laden"
+    }
+    val loadDataButtonText: LiveData<String> = _loadDataButtonText
+
+    private val _establishConnectionButtonText = MutableLiveData<String>().apply {
+        value = "Serververbindung aufbauen"
+    }
+    val establishConnectionButtonText: LiveData<String> = _establishConnectionButtonText
+
+    private val _startTrainingButtonText = MutableLiveData<String>().apply {
+        value = "Training starten"
+    }
+    val startTrainingButtonText: LiveData<String> = _startTrainingButtonText
+
+    // endregion
+
+    // region images
+
+    private val _loadDataImageDrawable = MutableLiveData<Int>().apply {
+        value = CROSS_DRAWABLE_ID
+    }
+    val loadDataImageDrawable: LiveData<Int> = _loadDataImageDrawable
+
+    private val _establishConnectionImageDrawable = MutableLiveData<Int>().apply {
+        value = CROSS_DRAWABLE_ID
+    }
+    val establishConnectionImageDrawable: LiveData<Int> = _establishConnectionImageDrawable
+
+    private val _startTrainingImageDrawable = MutableLiveData<Int>().apply {
+        value = CROSS_DRAWABLE_ID
+    }
+    val startTrainingImageDrawable: LiveData<Int> = _startTrainingImageDrawable
+
+    // endregion
+
+    // region button commands
+
+    fun handleLoadDataButton() {
+        _loadDataImageDrawable.value = CHECKMARK_DRAWABLE_ID
+    }
+
+    fun handleEstablishConnectionButton() {
+        _establishConnectionImageDrawable.value = CHECKMARK_DRAWABLE_ID
+    }
+
+    fun handleStartTrainingButton() {
+        _startTrainingImageDrawable.value = CHECKMARK_DRAWABLE_ID
+    }
+
+    // endregion
 }
